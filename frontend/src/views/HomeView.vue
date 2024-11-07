d056d2ea4f99d4bbebd06f5673d3dc3f5c93a0d5
RealChoki <rafaatchoki1999@gmail.com>
2024-11-07 23:01:06 +0100

    Enhance HomeView.vue: Update input layout, add full-screen overlay, and improve styling

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,60 +5,96 @@
         class="container-fluid d-flex justify-content-between align-items-center"
       >
         <!-- Burger icon -->
-        <img src="../components/icons/Menu.png" />
-        
-        <div class="calendar-days-background">
-          <font-awesome-icon :icon="['fas', 'calendar-days']"
-          class="calendar-days" 
+        <img src="../components/icons/Menu.png"
+          style="cursor: pointer;" />
+
+        <div class="calendar-days-background glow-on-hover">
+          <font-awesome-icon
+            :icon="['fas', 'calendar-days']"
+            class="calendar-days"
           />
         </div>
 
-        <font-awesome-icon :icon="['fas', 'pen-to-square']" 
-        class="pen-to-square"
-        style="color: #5b5b5b;"
+        <font-awesome-icon
+          :icon="['fas', 'pen-to-square']"
+          class="pen-to-square"
+          style="color: #5b5b5b; cursor: pointer;"
+          @click="reloadPage"
         />
-        
       </div>
     </nav>
 
     <div class="container my-4 logo-container">
       <img class="" src="../components/icons/dontSueChatGPT.png" />
-      
     </div>
 
     <!-- Sticky input bar at the bottom -->
-    <div class="d-flex justify-content-center gap-2 sticky-footer mb-3">
-      <font-awesome-icon
-        :icon="['fas', 'plus']"
-        class="cursor-pointer btn-circle"
-        style="background-color: #5b5b5b;"
-      />
+    <div class="sticky-footer navbar navbar-expand-lg navbar-light">
+      <div
+        class="container-fluid d-flex justify-content-center align-items-start gap-2"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'plus']"
+          class="cursor-pointer btn-circle align-bottom"
+          style="background-color: #5b5b5b"
+        />
 
-      <input
-        type="text"
-        class="custom-input"
-        placeholder="Type a message..."
-        aria-label="Message input"
-        v-model="message"
-      />
+        <textarea
+          class="custom-input"
+          placeholder="Type a message..."
+          aria-label="Message input"
+          v-model="message"
+          @input="resizeTextarea"
+        ></textarea>
 
-      <!-- Conditional Send/Voice icon with circular style -->
-      <font-awesome-icon
-        v-if="message"
-        :icon="['fas', 'arrow-up']"
-        class="cursor-pointer btn-circle bg-light"
-      />
-      <font-awesome-icon
-        v-else
-        :icon="['fas', 'volume-high']"
-        class="cursor-pointer btn-circle bg-light"
-      />
+        <div class="input-actions align-bottom">
+          <font-awesome-icon
+            v-if="message"
+            :icon="['fas', 'arrow-up']"
+            class="cursor-pointer btn-circle bg-light align-bottom"
+          />
+          <font-awesome-icon
+            v-else
+            :icon="['fas', 'volume-high']"
+            class="cursor-pointer btn-circle bg-light align-bottom"
+          />
+          <!-- </transition> -->
+          <font-awesome-icon
+            v-if="lineCount >= 3"
+            :icon="['fas', 'up-right-and-down-left-from-center']"
+            class="align-bottom custom-expand-btn"
+            @click="toggleOverlay"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Full-screen overlay -->
+  <div v-if="isOverlayVisible" class="full-screen-overlay">
+    <div class="height-100 p-2" style="background-color: #232323">
+      <div class="container h-100 d-flex flex-column">
+        <div class="d-flex justify-content-end mt-2 mb-3">
+          <font-awesome-icon
+            :icon="['fas', 'down-left-and-up-right-to-center']"
+            @click.stop="toggleOverlay"
+            class="collapse-icon"
+          />
+        </div>
+
+        <textarea
+          class="full-screen-textarea"
+          style="background-color: #232323"
+          placeholder="Type a message..."
+          aria-label="Message input"
+          v-model="message"
+        ></textarea>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -69,29 +105,54 @@ import {
   faVolumeHigh,
   faPenToSquare,
   faArrowRightLong,
-  faCalendarDays
+  faCalendarDays,
+  faUpRightAndDownLeftFromCenter,
+  faDownLeftAndUpRightToCenter,
 } from "@fortawesome/free-solid-svg-icons";
 
 // Add icons to the library
-library.add(faBars, faPlus, faArrowUp, faLeaf, faVolumeHigh, faPenToSquare, faArrowRightLong, faCalendarDays);
-
+library.add(
+  faBars,
+  faPlus,
+  faArrowUp,
+  faLeaf,
+  faVolumeHigh,
+  faPenToSquare,
+  faArrowRightLong,
+  faCalendarDays,
+  faUpRightAndDownLeftFromCenter,
+  faDownLeftAndUpRightToCenter
+);
 // Reactive state for message input
 const message = ref("");
+
+const isOverlayVisible = ref(false);
+
+const toggleOverlay = () => {
+  isOverlayVisible.value = !isOverlayVisible.value;
+  console.log(isOverlayVisible.value);
+};
+
+const lineCount = computed(() => {
+  return message.value.split("\n").length;
+});
+
+const resizeTextarea = (event: any) => {
+  const target = event.target;
+  target.style.height = "45px";
+  target.style.height = `${Math.min(target.scrollHeight, 200)}px`;
+};
+
+const reloadPage = () => {
+  location.reload();
+};
 </script>
 
 <style scoped>
-
-.custom-input {
-  width: 80%;
-  background-color: #232323;
-  border: none;
-  color: white;
-  border-radius: 25px;
-  height: 45px;
-}
-
 .custom-input::placeholder {
-  padding-left: 15px;
+  color: white;
+  padding: 0px;
+  margin: 0px;
 }
 
 .custom-input:focus {
@@ -129,13 +190,12 @@ const message = ref("");
   right: 0;
 }
 
-
 .sticky-footer {
   position: fixed;
   bottom: 0;
   left: 0;
-  width: 100%;           
-  padding: 1rem;           
+  width: 100%;
+  padding: 1rem;
   z-index: 1000;
 }
 
@@ -154,7 +214,7 @@ const message = ref("");
   color: #717171;
 }
 
-.calendar-days-background{
+.calendar-days-background {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -164,4 +224,82 @@ const message = ref("");
   border-radius: 20px;
   cursor: pointer;
 }
+
+.align-bottom {
+  align-self: flex-end;
+}
+
+.custom-input {
+  flex: 1;
+  background-color: #232323;
+  border: none;
+  color: white;
+  border-radius: 25px;
+  height: 45px;
+  max-height: 200px;
+  resize: none;
+  padding-top: 8px;
+  padding-left: 15px;
+}
+
+.input-actions {
+  position: relative;
+}
+
+.collapse-icon {
+  align-self: flex-end;
+  font-size: 1.5rem;
+  color: white;
+  cursor: pointer;
+  z-index: 10000;
+}
+
+.custom-expand-btn {
+  font-size: 0.9rem;
+  color: white;
+  position: absolute;
+  right: 14px;
+  top: -27px;
+  cursor: pointer;
+}
+
+.full-screen-textarea {
+  all: unset;
+  background-color: #232323;
+  color: white;
+  font-family: inherit;
+  font-size: 16px;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  resize: none;
+  outline: none;
+  border: none;
+  box-sizing: border-box;
+  overflow-y: auto;
+  line-height: 1.5;
+}
+
+.full-screen-textarea::placeholder {
+  color: #bbb;
+  opacity: 1;
+}
+
+.height-100 {
+  height: 100%;
+}
+
+.full-screen-overlay {
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 9999;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
 </style>
